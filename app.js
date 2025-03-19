009292cd19fab852cc1cdbd31020e2e7a7737b9a
Paulo <boy_pa_22@hotmail.com>
2025-03-18 22:36:12 -0500

    Lógica del desafio

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+    const nombreNormalizado = nombre.toLowerCase();
+    const existe = amigos.some(amigo => amigo.toLowerCase() === nombreNormalizado);
+    if (existe) {
+        alert("Este nombre ya está en la lista.");
+        return;
+    }
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    amigos.forEach((amigo) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Agrega al menos un nombre antes de sortear.");
+        return;
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSecreto = amigos[indiceAleatorio];
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSecreto}</strong> 🎉</li>`;
+}
